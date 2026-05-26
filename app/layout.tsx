b0f5fb23cd9ae2199a39b2e6883772da9298bc3e
Vemesh Bypureddi <158151118+Vemesh12@users.noreply.github.com>
2026-05-26 12:19:49 +0530

    for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>{children}</body>
+</html>
   );
 }
